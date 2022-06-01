eb5baf4495e64e75e3740b0708ad2c1aed877129
Ibrahim Khalil <rahatibrahim22@gmail.com>
2022-06-01 16:59:02 +0600

    commit

--- RemoveElement.java
+++ RemoveElement.java
@@ -1,9 +1,33 @@
 public class RemoveElement {
     public int removeElement(int[] nums, int val) {
         int k = 0;
-        for (int i = 0; i < nums.length; i++) {
-            
+        int lp = nums.length - 1;
+
+        for (int i = 0; i <= lp; i++) {
+            if (nums[i] == val) {
+                while (lp >= 0 && nums[lp] == val) {
+                    nums[lp] = -1;
+                    k++;
+                    lp -= 1;
+                }
+                if (lp < 0 || lp < i) {
+                    return nums.length - k;
+                }
+                nums[i] = nums[lp];
+                nums[lp] = -1;
+                k++;
+                lp -= 1;
+            }
         }
-        return k;
+        return nums.length - k;
+    }
+
+    public static void main(String[] args) {
+        RemoveElement e = new RemoveElement();
+        int[] a = {4};
+        System.out.println(e.removeElement(a, 4));
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
     }
 }
