bb0721baa643a09bb8e122819ab84ca52415e818
Ibrahim Khalil <rahatibrahim22@gmail.com>
2022-06-10 17:13:57 +0600

    flash

---
+++ MinMaxGame.java
@@ -0,0 +1,25 @@
+public class MinMaxGame {
+    public int minMaxGame(int[] nums) {
+        int n = nums.length;
+
+        while (n > 1) {
+            int[] newNums = new int[n / 2];
+            for (int i = 0; i < n / 2; i++) {
+                if (i % 2 == 0) {
+                    newNums[i] = Math.min(nums[2 * i], nums[2 * i + 1]);
+                } else {
+                    newNums[i] = Math.max(nums[2 * i], nums[2 * i + 1]);
+                }
+            }
+            nums = newNums.clone();
+            n = nums.length;
+        }
+
+        return nums[0];
+    }
+
+    public static void main(String[] args) {
+        MinMaxGame obj = new MinMaxGame();
+        System.out.println(obj.minMaxGame(new int[] {3}));
+    }
+}
