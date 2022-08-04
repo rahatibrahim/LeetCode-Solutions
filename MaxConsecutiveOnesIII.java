1b56dff618d9b684270920b3c1ee79c0e96c7d5a
Ibrahim Khalil <rahatibrahim22@gmail.com>
2022-08-05 03:06:59 +0600

    ...

--- MaxConsecutiveOnesIII.java
+++ MaxConsecutiveOnesIII.java
@@ -1,7 +1,19 @@
 public class MaxConsecutiveOnesIII {
     public int longestOnes(int[] nums, int k) {
-        
+        int start = 0, end = 0, counter = 0, len = 0;
 
-        return 0;
+        while (end < nums.length) {
+            if (nums[end] == 0) counter++;
+            end++;
+
+            while (counter > k) {
+                if (nums[start] == 0) counter--;
+                start++;
+            }
+
+            len = Math.max(len, end - start);
+        }
+
+        return len;
     }
 }
--- Test.java
+++ Test.java
@@ -1,9 +1,23 @@
 public class Test {
-    public static void main(String[] args) {
-        int[] f = { 1, 3, 4, 5, 3 };
-
-        for (int i : f) {
-            System.out.println(i);
+    public static String removeDigit(String number, char digit) {
+        String max = "0";
+        String curr = "0";
+        StringBuilder numArray = new StringBuilder(number);
+        for (int i = 0; i < number.length(); i++) {
+            if (number.charAt(i) == digit) {
+                numArray.deleteCharAt(i);
+                curr = numArray.toString();
+                if(max.compareTo(curr) > 0)
+                    max = curr;
+                numArray.insert(i, number.charAt(i));
+            }
         }
+        
+        return max;
+    }
+
+    public static void main(String[] args) {
+        String str = "1231";
+        System.out.println(removeDigit(str, '1'));
     }
 }
