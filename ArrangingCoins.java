b5d393e1b8eb20287c5988b69569533d1073d540
Ibrahim Khalil <rahatibrahim22@gmail.com>
2022-08-10 02:06:08 +0600

    solved

---
+++ ArrangingCoins.java
@@ -0,0 +1,24 @@
+public class ArrangingCoins {
+    public static int arrangeCoins(int n) {
+        int l = 1, r = n;
+        int row = 0;
+        
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            int coins = mid * (mid + 1) / 2;
+            
+            if (coins > n) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+                row = mid;
+            }
+        }
+        
+        return row;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(arrangeCoins(1));
+    }
+}
